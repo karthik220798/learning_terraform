ba29d588cd5523391d87017c661d6197656db724
karthik220798 <77311983+karthik220798@users.noreply.github.com>
2023-11-25 20:59:31 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,13 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+resource "aws_instance" "" {
+  ami           = var.ami
+  instance_type = var.instance_type
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  network_interface {
+    network_interface_id = var.network_interface_id
+    device_index         = 0
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  tags = {
-    Name = "HelloWorld"
+  credit_specification {
+    cpu_credits = "unlimited"
   }
 }
